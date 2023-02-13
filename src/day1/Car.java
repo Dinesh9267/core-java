a4a5b8165f2cd530c230e88162e2c6a1b53c3147
Dinesh9267 <dinesh.99joshi@gmail.com>
2023-02-13 18:16:12 -0500

    learning about OOP Class

---
+++ src/day1/Car.java
@@ -0,0 +1,22 @@
+package day1;
+
+import java.awt.*;
+
+public class Car {
+
+    // State
+
+    // Brand
+    // Color
+    // Weight
+    // Interior
+    // Exterior
+    // Hybrid
+    // Audio Multimedia
+    // Connected Services
+    // Price
+
+
+
+
+}
---
+++ src/day1/Television.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class Television {
+
+    // State
+
+    // Size
+    // Type
+    // Brand
+    // Price
+    // Screen Type
+    // Feature
+
+
+
+    // Behaviour
+}
